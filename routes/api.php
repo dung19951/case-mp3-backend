54c9fca12d823db53a6f55c2ef22e767bd59c25f
nguyenngoc-it <nguyenngocit.na@gmail.com>
2021-09-03 15:39:03 +0700

    api singercontroller

--- routes/api.php
+++ routes/api.php
@@ -24,13 +24,31 @@ Route::prefix('category')->group(function (){
     Route::post('{id}/update',[CategoryController::class,'update']);
     Route::delete('{id}/delete',[CategoryController::class,'destroy']);
 });
-Route::prefix('singer')->group(function (){
-    Route::get('/',[SingerController::class,'getAll']);
-    Route::post('/store',[SingerController::class,'add']);
-    Route::post('{id}/update',[SingerController::class,'update']);
-    Route::delete('{id}/delete',[SingerController::class,'destroy']);
+//Route::prefix('singer')->group(function (){
+//    Route::get('/',[SingerApiController::class,'getAllSinger']);
+//    Route::post('/store',[SingerApiController::class,'store']);
+//    Route::post('/update/{id}',[SingerApiController::class,'updateSinger']);
+//    Route::delete('/delete/{id}',[SingerApiController::class,'deleteSinger']);
+//    Route::get('detail/{id}',[SingerApiController::class,'singerDetail']);
+//    Route::get('songs/{id}',[SingerApiController::class,'getListSongBySinger']);
+//    Route::get('/find/',[SingerApiController::class,'findSinger']);
+Route::prefix('singer')->group(function () {
+    Route::get('/', [SingerController::class, 'getAll']);
+    Route::post('/store', [SingerController::class, 'add']);
+    Route::post('{id}/update', [SingerController::class, 'update']);
+    Route::delete('{id}/delete', [SingerController::class, 'destroy']);
 });
 
 
-Route::post('register',[UserController::class,'register']);
-Route::post('login',[UserController::class,'login']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::post('forgot', [UserController::class, 'forgot']);
+Route::post('reset', [UserController::class, 'reset']);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('logout', [UserController::class, 'logout']);
+    Route::prefix('users')->group(function () {
+        Route::get('/{id}', [UserController::class, 'getUserProfileById']);
+        Route::post('/{id}/update', [UserController::class, 'updateUser']);
+        Route::post('/{id}/delete', [UserController::class, 'getUserProfileById']);
+    });
+});
