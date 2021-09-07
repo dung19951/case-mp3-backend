50c80b67dd9ad70c7b62a09ee639b025981a6cd0
locvankhoic1020g1 <locvankhoi@gmail.com>
2021-09-07 11:16:57 +0700

    sua laij routes

--- routes/api.php
+++ routes/api.php
@@ -42,13 +42,7 @@ Route::group(['middleware' => 'auth:api'], function () {
         Route::post('/{id}/update', [UserController::class, 'updateUser']);
         Route::post('/{id}/delete', [UserController::class, 'getUserProfileById']);
     });
-    Route::prefix('category')->group(function (){
-        Route::get('/',[CategoryController::class,'gelAll']);
-        Route::post('store',[CategoryController::class,'store']);
-        Route::post('{id}/update',[CategoryController::class,'update']);
-        Route::delete('{id}/delete',[CategoryController::class,'destroy']);
-    });
-
+});
 Route::prefix('category')->group(function () {
     Route::get('/', [CategoryController::class, 'gelAll']);
     Route::post('store', [CategoryController::class, 'store']);
@@ -56,29 +50,10 @@ Route::prefix('category')->group(function () {
     Route::delete('{id}/delete', [CategoryController::class, 'destroy']);
 });
 
-Route::prefix('singer')->group(function (){
-    Route::get('/',[SingerApiController::class,'getAllSinger']);
-    Route::post('/store',[SingerApiController::class,'store']);
-/*    Route::post('/update/{id}',[SingerApiController::class,'updateSinger']);*/
-/*    Route::delete('/delete/{id}',[SingerApiController::class,'deleteSinger']);*/
-    Route::get('detail/{id}',[SingerApiController::class,'singerDetail']);
-    Route::get('songs/{id}',[SingerApiController::class,'getListSongBySinger']);
-    Route::get('/find/',[SingerApiController::class,'findSinger']);
 Route::prefix('singer')->group(function () {
-    Route::get('/', [SingerController::class, 'getAll']);
-    Route::post('/store', [SingerController::class, 'add']);
-    Route::post('{id}/update', [SingerController::class, 'update']);
-    Route::delete('{id}/delete', [SingerController::class, 'destroy']);
-});
-
-
-Route::post('register', [UserController::class, 'register']);
-Route::post('login', [UserController::class, 'login']);
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('logout', [UserController::class, 'logout']);
-    Route::prefix('users')->group(function () {
-        Route::get('/{id}', [UserController::class, 'getUserProfileById']);
-        Route::post('/{id}/update', [UserController::class, 'updateUser']);
-        Route::post('/{id}/delete', [UserController::class, 'getUserProfileById']);
-    });
+    Route::get('/', [SingerApiController::class, 'getAllSinger']);
+    Route::post('/store', [SingerApiController::class, 'store']);
+    Route::get('detail/{id}', [SingerApiController::class, 'singerDetail']);
+    Route::get('songs/{id}', [SingerApiController::class, 'getListSongBySinger']);
+    Route::get('/find/', [SingerApiController::class, 'findSinger']);
 });
