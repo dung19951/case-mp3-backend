73f3390c45dd8249d65c9c17fdecd5dd4dda8e7e
nguyenngoc-it <nguyenngocit.na@gmail.com>
2021-09-07 11:32:56 +0700

    merge api

--- database/migrations/2021_09_07_032225_create_user_id_in_song_table.php
+++ database/migrations/2021_09_07_032225_create_user_id_in_song_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateForeinKeyOnRolesAndSongsTable extends Migration
+class CreateUserIdInSongTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateForeinKeyOnRolesAndSongsTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->foreign('role_id')->references('id')->on('roles');
+        Schema::table('songs', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
@@ -25,7 +26,7 @@ class CreateForeinKeyOnRolesAndSongsTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('songs', function (Blueprint $table) {
             //
         });
     }
